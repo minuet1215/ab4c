cdf11c6e03a50f7912d73fb9453ba25aa155f7ca
YoungCheon <kakao_story@naver.com>
2022-07-20 15:35:34 +0900

    socket component 정리

--- client/src/components/GroupPage/GroupPage.js
+++ client/src/components/GroupPage/GroupPage.js
@@ -1,149 +1,18 @@
 import React, { useEffect, useState, useRef } from "react";
-import { Typography } from "antd";
 import styles from "./GroupPage.module.css";
-import remove from "./remove.js";
-import remove2 from "./remove2.js";
-import io from "socket.io-client";
 import html2canvas from "html2canvas";
-import url from "socket.io-client/lib/url";
 import makeGif from "./makeGIF.js";
-
-import CameraTabs from "./CameraTab";
-import CameraBottom from "./CameraBottom";
+import Socket from "./Socket";
 
 function GroupPage() {
+  const localVideoRef = useRef(null);
   let IMGS = new Array();
-  const { Title } = Typography;
-  let roomName = "123455";
-  let [leave, setLeave] = useState(true);
+  let roomName = "123455"; //룸이름
   const [ImgBase64, setImgBase64] = useState(""); // 업로드 될 이미지
   const [imgFile, setImgFile] = useState(null); // 파일 전송을 위한 state
-  let isMute = false;
-  const pc_config = {
-    iceServers: [
-      {
-        urls: [
-          "stun:stun.l.google.com:19302",
-          "stun:stun1.l.google.com:19302",
-          "stun:stun2.l.google.com:19302",
-          "stun:stun3.l.google.com:19302",
-          "stun:stun4.l.google.com:19302",
-        ],
-      },
-    ],
-  };
-  const SOCKET_SERVER_URL = "http://localhost:5001"; // ! : local
-  // const SOCKET_SERVER_URL = "http://www.4cut.shop"; // ! : dev
-  const socketRef = useRef();
-  const pcRef = useRef();
-  const localVideoRef = useRef(null);
-  const remoteVideoRef = useRef(null);
-  const setVideoTracks = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true,
-      });
-      if (localVideoRef.current) localVideoRef.current.srcObject = stream;
-      if (!(pcRef.current && socketRef.current)) return;
-      stream.getTracks().forEach((track) => {
-        if (!pcRef.current) return;
-        pcRef.current.addTrack(track, stream);
-      });
-      pcRef.current.onicecandidate = (e) => {
-        if (e.candidate) {
-          if (!socketRef.current) return;
-          console.log("onicecandidate");
-          socketRef.current.emit("candidate", e.candidate);
-        }
-      };
-      pcRef.current.oniceconnectionstatechange = (e) => {
-        console.log(e);
-      };
-      pcRef.current.ontrack = (ev) => {
-        console.log("add remotetrack success");
-        if (remoteVideoRef.current) {
-          remoteVideoRef.current.srcObject = ev.streams[0];
-          setLeave(false);
-        }
-      };
-      socketRef.current.emit("join_room", {
-        room: roomName,
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  };
-  const createOffer = async () => {
-    console.log("create offer");
-    if (!(pcRef.current && socketRef.current)) return;
-    try {
-      const sdp = await pcRef.current.createOffer({
-        offerToReceiveAudio: true,
-        offerToReceiveVideo: true,
-      });
-      await pcRef.current.setLocalDescription(new RTCSessionDescription(sdp));
-      socketRef.current.emit("offer", sdp);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-  const createAnswer = async (sdp) => {
-    if (!(pcRef.current && socketRef.current)) return;
-    try {
-      await pcRef.current.setRemoteDescription(new RTCSessionDescription(sdp));
-      console.log("answer set remote description success");
-      const mySdp = await pcRef.current.createAnswer({
-        offerToReceiveVideo: true,
-        offerToReceiveAudio: true,
-      });
-      console.log("create answer");
-      await pcRef.current.setLocalDescription(new RTCSessionDescription(mySdp));
-      socketRef.current.emit("answer", mySdp);
-    } catch (e) {
-      console.error(e);
-    }
-  };
+  let isMute = false; // 음소거 변수
 
-  useEffect(() => {
-    socketRef.current = io.connect(SOCKET_SERVER_URL);
-    pcRef.current = new RTCPeerConnection(pc_config);
-    socketRef.current.on("all_users", (allUsers) => {
-      if (allUsers.length > 0) {
-        createOffer();
-      }
-    });
-    socketRef.current.on("getOffer", (sdp) => {
-      //console.log(sdp);
-      console.log("get offer");
-      createAnswer(sdp);
-    });
-    socketRef.current.on("getAnswer", (sdp) => {
-      console.log("get answer");
-      if (!pcRef.current) return;
-      pcRef.current.setRemoteDescription(new RTCSessionDescription(sdp));
-    });
-    socketRef.current.on("getCandidate", async (candidate) => {
-      if (!pcRef.current) return;
-      await pcRef.current.addIceCandidate(new RTCIceCandidate(candidate));
-      console.log("candidate add success");
-    });
-    socketRef.current.on("user_exit", (e) => {
-      setLeave(true);
-      console.log("나감");
-    });
-    setVideoTracks();
-    remove();
-    remove2();
-    return () => {
-      if (socketRef.current) {
-        socketRef.current.disconnect();
-      }
-      if (pcRef.current) {
-        pcRef.current.close();
-      }
-    };
-  }, []);
+  // 캡쳐하는 함수
   function onCapture() {
     html2canvas(document.querySelector("#capture"), {
       allowTaint: false,
@@ -156,7 +25,7 @@ function GroupPage() {
       IMGS.push(DATA_URL);
     });
   }
-  //로컬 저장하는 함수, 안씀
+  //로컬 저장하는 함수, 아직은 안씀
   const OnSaveAs = (uri, filename) => {
     let link = document.createElement("a");
     if (typeof link.download === "string") {
@@ -169,6 +38,7 @@ function GroupPage() {
       window.open(uri);
     }
   };
+  // 음소거 버튼
   function changeMuteButton() {
     isMute = !isMute;
     localVideoRef.current.muted = isMute;
@@ -221,50 +91,18 @@ function GroupPage() {
         Make a Gif
       </button>
       <input type="file" id="imgFile" onChange={handleChangeFile} />
-      <div>
-        <div
-          className={styles.box}
-          id="capture"
-          style={{
-            backgroundImage: ImgBase64
-              ? `url(${ImgBase64})`
-              : "url(https://image.jtbcplus.kr/data/contents/jam_photo/202103/31/381e8930-6c3a-440f-928f-9bc7245323e0.jpg)",
-          }}
-        >
-          <canvas className={styles.mirror} id="mytrans"></canvas>
-          <canvas
-            className={leave ? styles.displaynone : styles.mirror}
-            id="remotetrans"
-          ></canvas>
-        </div>
-        <div className="camera_tab">
-          <CameraTabs />
-        </div>
-        <div className="camera_bottom">
-          <CameraBottom />
-        </div>
+      <div
+        className={styles.box}
+        id="capture"
+        style={{
+          backgroundImage: ImgBase64
+            ? `url(${ImgBase64})`
+            : "url(https://image.jtbcplus.kr/data/contents/jam_photo/202103/31/381e8930-6c3a-440f-928f-9bc7245323e0.jpg)",
+        }}
+      >
+        <Socket roomName={roomName} ref={localVideoRef}></Socket>
       </div>
       <img id="result-image" />
-      <video
-        className={styles.displaynone}
-        id="my_face"
-        autoPlay
-        playsInline
-        width="640"
-        height="480"
-        ref={localVideoRef}
-      ></video>
-      <video
-        className={styles.displaynone}
-        id="remote"
-        autoPlay
-        playsInline
-        width="640"
-        height="480"
-        ref={remoteVideoRef}
-      ></video>
-      <canvas className={styles.displaynone} id="remotegreen"></canvas>
-      <canvas className={styles.displaynone} id="mygreen"></canvas>
     </div>
   );
 }
