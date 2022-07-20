96d4b3cb7c8d103b0568f218cb6cd6a4dc427c78
sunnie-han <91239986+sunnie-han@users.noreply.github.com>
2022-07-20 21:10:12 +0900

    css 작업 1차: langind, login, main 위주

--- client/src/components/LobbyPage/LobbyPage.js
+++ client/src/components/LobbyPage/LobbyPage.js
@@ -4,91 +4,56 @@ import { useNavigate } from "react-router-dom";
 import { enterRoom } from "../../_actions/room_action";
 import { Layout, Menu, Form, Input, Button } from "antd";
 import MyHeader from "../Header/Header";
+import styles from "./LobbyPage.module.css";
 
 function LobbyPage() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { Header, Content, Footer } = Layout;
 
-  const [RoomNumber, setRoomNumber] = useState("");
-  const onRoomNumberHandler = (event) => {
-    setRoomNumber(event.currentTarget.value);
-  };
+  // const [RoomNumber, setRoomNumber] = useState("");
+  // const onRoomNumberHandler = (event) => {
+  //   setRoomNumber(event.currentTarget.value);
+  // };
 
-  const onEnterRoomBtnHandler = (event) => {
-    event.preventDefault();
+  // const onEnterRoomBtnHandler = (event) => {
+  //   event.preventDefault();
 
-    console.log(RoomNumber);
-    if (!RoomNumber) {
-      return alert("> 그룹에 참가할 수 없습니다.");
-    }
+  //   console.log(RoomNumber);
+  //   if (!RoomNumber) {
+  //     return alert("> 그룹에 참가할 수 없습니다.");
+  //   }
 
-    let body = {
-      roomNumber: RoomNumber,
-    };
+  //   let body = {
+  //     roomNumber: RoomNumber,
+  //   };
 
-    dispatch(enterRoom(body)).then((response) => {
-      if (response.payload.success) {
-        navigate("/video_call/" + body.roomNumber); // 페이지 이동
-      } else {
-        alert("그룹에 참가할 수 없습니다.");
-      }
-    });
-  };
+  //   dispatch(enterRoom(body)).then((response) => {
+  //     if (response.payload.success) {
+  //       navigate("/video_call/" + body.roomNumber); // 페이지 이동
+  //     } else {
+  //       alert("그룹에 참가할 수 없습니다.");
+  //     }
+  //   });
+  // };
   return (
-    <div>
-      <MyHeader subTitle="메인 화면" />
-
-      <Layout>
-        <Header>
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={["1"]}
-            items={new Array(5).fill(null).map((_, index) => {
-              const key = index + 1;
-              return {
-                key,
-                label: `nav ${key}`,
-              };
-            })}
-          />
-        </Header>
-
-        <Content style={{ padding: "50px 50px" }}>
-          <Form
-            layout="inline"
-            style={{ padding: "24px", justifyContent: "center" }}
+    <div className="container">
+      <MyHeader subTitle="대기실" onBackUrl="/main" />
+      <div
+        className="contents_container center"
+        style={{ paddingBottom: "5%" }}
+      >
+        <h1>멤버</h1>
+        <div className={styles.member_container}></div>
+        <div style={{ paddingBottom: "5%" }}>
+          <button
+            className="button_gap btn_1"
+            onClick={() => navigate("/group")}
           >
-            <Form.Item
-              label="그룹 번호"
-              rules={[
-                {
-                  required: true,
-                  message: "그룹 번호를 입력해주세요.",
-                },
-              ]}
-            >
-              <Input value={RoomNumber} onChange={onRoomNumberHandler} />
-            </Form.Item>
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                onClick={onEnterRoomBtnHandler}
-              >
-                그룹 참가
-              </Button>
-            </Form.Item>
-          </Form>
-          <div
-            style={{ minHeight: "280px", padding: "24px", background: "#fff" }}
-          >
-            내용 없음
-          </div>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>안방 네컷</Footer>
-      </Layout>
+            입장
+          </button>
+          <button className="button_gap btn_2">초대</button>
+        </div>
+      </div>
     </div>
   );
 }
