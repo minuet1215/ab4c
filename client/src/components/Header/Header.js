185921951f151c173d2435bf9f66b9896f917fff
YoungCheon <kakao_story@naver.com>
2022-07-23 23:36:21 +0900

    뒤로 가면 카메라 안꺼지는 오류 수정

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,12 +2,15 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { PageHeader } from "antd";
 
-function Header({ subTitle, onBackUrl = "/" }) {
+function Header({ subTitle, onBackUrl = "/", onClick = undefined }) {
   const navigate = useNavigate();
   return (
     <PageHeader
       style={{ border: "1px solid rgb(235, 237, 240)" }}
-      onBack={() => navigate(onBackUrl)}
+      onBack={() => {
+        if (onClick != undefined) onClick();
+        navigate(onBackUrl);
+      }}
       title="안방 네컷"
       subTitle={subTitle}
     />
