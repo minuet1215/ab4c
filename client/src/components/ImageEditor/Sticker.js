fdbe7a0821fb719061ce0a3d259965b8592a7abb
baedonguri <rmsdbraos21@naver.com>
2022-07-26 14:42:50 +0900

    Image Editor 스티커 추가 및 PWA splash Image 개선

--- client/src/components/ImageEditor/Sticker.js
+++ client/src/components/ImageEditor/Sticker.js
@@ -6,12 +6,17 @@ export default function StickerRow({ onStickerSelected }) {
     "https://cdn-icons-png.flaticon.com/128/149/149456.png",
     "https://cdn-icons-png.flaticon.com/128/159/159117.png",
     "https://cdn-icons-png.flaticon.com/128/159/159108.png",
-    "https://cdn-icons-png.flaticon.com/128/7870/7870571.png",
+    "https://cdn-icons-png.flaticon.com/128/24/24674.png",
+    "https://cdn-icons-png.flaticon.com/128/2214/2214904.png",
     "https://cdn-icons-png.flaticon.com/128/744/744546.png",
     "https://cdn-icons-png.flaticon.com/128/5259/5259457.png",
     "https://cdn-icons-png.flaticon.com/128/6532/6532955.png",
+    "https://cdn-icons-png.flaticon.com/256/7871/7871411.png",
+    "https://cdn-icons-png.flaticon.com/256/7871/7871429.png",
+    "https://cdn-icons-png.flaticon.com/256/7871/7871500.png",
+    "https://cdn-icons-png.flaticon.com/256/7871/7871518.png",
   ];
-
+  
   return (
     <>
       <div className="sticker-container">
