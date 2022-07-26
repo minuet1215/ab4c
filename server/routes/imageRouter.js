fa9cb944e4718787fe0a23b8c5899ad1ef5417e2
sunnie-han <91239986+sunnie-han@users.noreply.github.com>
2022-07-27 02:02:56 +0900

    앨범 정렬 최신순으로 수정, 촬영화면 불필요한 중간 탭 삭제

--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -29,14 +29,16 @@ imageRouter.post("/post", upload.single("file"), async (req, res) => {
 
 imageRouter.post("/album", async (req, res) => {
   // public 이미지들만 제공
-  const images = await Image.find({ public: "true" }); // 탐색, 수정, 옵션
+  const images = await Image.find({ public: "true" }).sort({
+    createdAt: -1,
+  }); // 탐색, 수정, 옵션
   res.json(images);
 });
 
 imageRouter.get("/album/top10", async (req, res) => {
   // public 이미지들만 제공, 좋아요 순, 최대 20개
   const images = await Image.find({ public: true })
-    .sort({ likes_count: -1 })
+    .sort({ likes_count: -1, createdAt: -1 })
     .limit(10); // 탐색, 수정, 옵션
   res.json(images);
 });
@@ -44,7 +46,9 @@ imageRouter.get("/album/top10", async (req, res) => {
 imageRouter.post("/album/me", async (req, res) => {
   // 내가 찍은 사진들 제공
   if (req.body.id) {
-    const images = await Image.find({ "user._id": req.body.id }); // 탐색, 수정, 옵션
+    const images = await Image.find({ "user._id": req.body.id }).sort({
+      createdAt: -1,
+    }); // 탐색, 수정, 옵션
     res.json(images);
   }
 });
@@ -125,5 +129,10 @@ imageRouter.patch("/:imageId/like", async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+
+// imageRouter.post("/test", multer, async (req, res) => {
+//   console.log(req.body);
+//   res.json({ message: "test" });
+// });
 
 module.exports = imageRouter;
