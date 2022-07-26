1c0947aa5e059118b6cfae2eaeaec1f40bf72b39
kimhyunjin <joice.kim7@gmail.com>
2022-07-26 22:09:31 +0900

    로딩 가운데로 오게 수정

--- client/src/components/Loading/Loading.js
+++ client/src/components/Loading/Loading.js
@@ -4,10 +4,8 @@ import loadingImg from "./loadingImg.gif";
 
 const Background = styled.div`
   position: absolute;
-  width: 100vw;
-  height: 100vh;
-  top: 0;
-  left: 0;
+  width: 100%;
+  height: 100%;
   background: #ffffffb7;
   z-index: 999;
   display: flex;
