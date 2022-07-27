957fb9cfc4cd3d03fe30bcb611f268643a73d747
kimhyunjin <joice.kim7@gmail.com>
2022-07-28 00:52:55 +0900

    카운트다운 구현

--- client/src/components/GroupPage/GroupPage.js
+++ client/src/components/GroupPage/GroupPage.js
@@ -12,6 +12,7 @@ import MuteBtn from "./MuteBtn";
 import CaptureBtn from "./CaptureBtn";
 import { toast } from "react-toastify";
 import cameraAudioSrc from "./audio/camera.mp3"; // 카메라 셔터 음원s
+import CountDown from "../CountDown/CountDown";
 
 let IMGS = [];
 let gifFrames = [[], [], [], [], [], [], []];
@@ -130,8 +131,17 @@ function GroupPage() {
           </div>
         </div>
         {startCapture ? (
-          <div className={styles.rest_container} id="countdown">
-            <p className={styles.count_down_text}>{Math.floor(countDown)}</p>
+          <div
+            className={styles.rest_container}
+            id="countdown"
+            style={{
+              justifyContent: "center",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            {/* <p className={styles.count_down_text}>{Math.floor(countDown)}</p> */}
+            <CountDown />
           </div>
         ) : roomname === token ? (
           <div className={styles.rest_container} id="cameratab">
